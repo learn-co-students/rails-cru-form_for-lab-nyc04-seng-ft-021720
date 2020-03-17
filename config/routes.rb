91381681ec04dded50aa684e504db2dbdd3d0a41
kev-kev <kevin.clark93@gmail.com>
2020-03-17 15:07:04 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :artists, only: [:index, :new, :show, :create, :edit, :update]
+  resources :genres, only: [:index, :new, :show, :create, :edit, :update]
+  resources :songs, only: [:index, :new, :show, :create, :edit, :update]
+
+  # patch 'artists/:id', to: 'artists#update'
+  # patch 'genre/:id', to: 'genre#update'
+  # patch 'songs/:id', to: 'songs#update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
